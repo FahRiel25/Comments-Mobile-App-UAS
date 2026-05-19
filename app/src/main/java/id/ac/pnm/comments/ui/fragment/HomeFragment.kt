69d3d901da29c44bfa7e144ca84f084d86be309f
TakeruNojima <nojimatakeru14@gmail.com>
2026-05-20 01:10:15 +0700

    Clean up code

--- app/src/main/java/id/ac/pnm/comments/ui/fragment/HomeFragment.kt
+++ app/src/main/java/id/ac/pnm/comments/ui/fragment/HomeFragment.kt
@@ -11,8 +11,6 @@ import com.google.firebase.auth.FirebaseAuth
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -21,52 +19,34 @@ private const val ARG_PARAM2 = "param2"
  */
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var tvUsername: TextView
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        val tvUsername = view.findViewById<TextView>(R.id.tvUsername)
+        initViews(view)
 
-        val user = FirebaseAuth.getInstance().currentUser
-
-        val username = user?.displayName
-
-        tvUsername.text = "Hello, $username"
+        loadUsername()
 
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun initViews(view: View) {
+        tvUsername = view.findViewById(R.id.tvUsername)
     }
+
+    private fun loadUsername() {
+        val user = FirebaseAuth.getInstance().currentUser
+
+        val username = user?.displayName ?: "User"
+
+        tvUsername.text = "Hello, $username"
+    }
+
 }
